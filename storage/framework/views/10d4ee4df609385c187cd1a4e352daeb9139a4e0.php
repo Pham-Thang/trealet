95b6a8b4af9e42de31211fa4283185512154a910
Huy <dangbahuy99@gmail.com>
2021-10-31 22:01:27 +0700

    fix something

---
+++ storage/framework/views/10d4ee4df609385c187cd1a4e352daeb9139a4e0.php
@@ -0,0 +1,5 @@
+<script>
+    var read_announcements_endpoint = "<?php echo e(route('announcements.read')); ?>";
+</script>
+<script src="<?php echo e(url("vendor/plugins/announcements/js/announcements.js")); ?>"></script>
+<?php /**PATH /home/huy/Desktop/trealet/trealet-project/vendor/vanguardapp/announcements/src/../resources/views/partials/scripts.blade.php ENDPATH**/ ?>
