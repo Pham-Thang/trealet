09a6308191833a3b7c7221676266afd0108a0d8c
Huy <dangbahuy99@gmail.com>
2021-11-03 01:22:47 +0700

    fix laravel mix

--- readme.md
+++ readme.md
@@ -2,17 +2,15 @@
 
 ## Installation
 
-Please check the official laravel installation guide for server requirements before you start. [Official Documentation](https://laravel.com/docs/7.x/installation#installation)
+Please check the official [Laravel](https://laravel.com/docs/7.x/installation#installation) installation guide 
+
+And check the official [Node](https://https://nodejs.org/en/docs/) installation guide for server requirements before you start 
 
 Alternative installation is possible without local dependencies relying on [Docker](#docker). 
 
-Clone the repository
-
-    git clone git@github.com:gothinkster/laravel-realworld-example-app.git
-
 Switch to the repo folder
 
-    cd laravel-realworld-example-app
+    cd /trealet-project
 
 Install all the dependencies using composer
 
@@ -22,6 +20,22 @@ Copy the example env file and make the required configuration changes in the .en
 
     cp .env.example .env
 
+Edit env file :
+-Run local : 
+
+    APP_ENV=local
+
+-Run production :
+
+    APP_ENV=production
+
+-Add your database : 
+
+    DB_HOST="host database server"
+    DB_DATABASE="database name"
+    DB_USERNAME="database root user"
+    DB_PASSWORD="password"
+
 Generate a new application key
 
     php artisan key:generate
@@ -30,11 +44,26 @@ Run the database migrations (**Set the database connection in .env before migrat
 
     php artisan migrate
 
-Start the local development server
+Install all package nodejs (**Project need Compiling Assets (Mix)**)
+
+    npm install
+
+Run Mix
+
+    npm run dev
+
+or production run :
+
+    npm run prod 
+
+Now, Start the local development server
 
     php artisan serve
 
 You can now access the server at http://localhost:8000
+
+
+(**Edit 03/11/2021 by dbh939**)
 
 ## Vanguard - Advanced PHP Login and User Management
 
---
+++ storage/framework/views/7f3de247a1a3573a2165ef7364982876b9ecc72b.php
@@ -0,0 +1,71 @@
+
+
+<?php $__env->startSection('page-title', 'Trealet plays'); ?>
+<?php $__env->startSection('page-heading', 'Trealet plays'); ?>
+
+<?php $__env->startSection('breadcrumbs'); ?>
+    <li class="breadcrumb-item active">
+        You have played the following Trealets, click to see the details.
+    </li>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('partials.messages', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+	
+	<div class="card"><div class="card-body">
+		<div class="table-responsive" id="users-table-wrapper">
+			<table class="table table-borderless table-striped">
+				<thead>
+				<tr>
+					<th class="min-width-80"><?php echo app('translator')->get('ID'); ?></th>
+					<th class="min-width-200"><?php echo app('translator')->get('Title'); ?></th>
+					<th class="min-width-80"><?php echo app('translator')->get('Played at'); ?></th>
+					<th class="text-center min-width-150"><?php echo app('translator')->get('Action'); ?></th>
+				</tr>
+				</thead>
+				<tbody>
+					<?php if(count($utts)): ?>
+						<?php $__currentLoopData = $utts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $utt): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<tr>
+						<td class="align-middle"><a href="trealet-play-details?tr=<?php echo e($utt->id_str); ?>"><?php echo e($utt->id_str); ?></a></td>
+						<td class="align-middle"><?php echo e($utt->title); ?> (<?php echo e($utt->username); ?>)</td>
+						<td class="align-middle"><?php echo e($utt->played_at->format('y-m-d h:m')); ?></td>
+						<td class="text-center align-middle">
+							<a href="#"
+							   class="btn btn-icon"
+							   title="<?php echo app('translator')->get('Delete Trealet'); ?>"
+							   data-toggle="tooltip"
+							   data-placement="top"
+							   data-method="DELETE"
+							   data-confirm-title="<?php echo app('translator')->get('Please Confirm'); ?>"
+							   data-confirm-text="<?php echo app('translator')->get('Are you sure that you want to delete this trealet?'); ?>"
+							   data-confirm-delete="<?php echo app('translator')->get('Yes, delete it!'); ?>">
+								<i class="fas fa-trash"></i>
+							</a>
+						</td>
+					</tr>
+						<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+					<?php else: ?>
+						<tr>
+							<td colspan="4"><em><?php echo app('translator')->get('No records found.'); ?></em></td>
+						</tr>
+					<?php endif; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('styles'); ?>
+    <style>
+        .user.media {
+            float: left;
+            border: 1px solid #dfdfdf;
+            background-color: #fff;
+            padding: 15px 20px;
+            border-radius: 4px;
+            margin-right: 15px;
+        }
+    </style>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/huy/Desktop/trealet/trealet-project/resources/views/trealets/trealet-plays.blade.php ENDPATH**/ ?>
