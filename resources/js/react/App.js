395faab4225f91e84dc9937fc090ef1d9ab6b6e0
Huy <dangbahuy99@gmail.com>
2021-11-22 16:13:11 +0700

    somethings

---
+++ resources/js/react/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import * as action from "./store/actions";
+import Maps from "./screens/Maps/Maps";
+import "./App.css";
+
+store.dispatch(action.authCheck());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/maps" component={Maps} />
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById("app")
+);
