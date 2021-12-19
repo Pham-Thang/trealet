4479aaae5d7ccf164f5ef0e6bac5aaf0a64413b9
Huy <dangbahuy99@gmail.com>
2021-12-20 03:46:56 +0700

    update maps play

--- resources/js/maps/screen/Maps/components/MapView.jsx
+++ resources/js/maps/screen/Maps/components/MapView.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import GoogleMap, { fitBounds } from "google-map-react";
+import GoogleMap from "google-map-react";
 import GpsIcon from "../../../components/icons";
 import { AimOutlined } from "@ant-design/icons";
 import Marker from "../../../components/Marker";
@@ -10,122 +10,249 @@ import { getDistanceFromLatLonInKm } from "../../../utils/util";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { message, Button } from "antd";
-
+import GoogleMapReact from "google-map-react";
+import Polyline from "./Polyline";
+import { Modal } from "rsuite";
 
 const MapView = (props) => {
-  const { maps, gps, isGps, ganNhat,km, center, zoom } = useSelector((state) => ({
-    maps: select(state, "mapsReducer", "maps"),
-    gps: select(state, "mapsReducer", "gps"),
-    isGps: select(state, "mapsReducer", "isGps"),
-    ganNhat: select(state, "mapsReducer", "ganNhat"),
-    km: select(state, "mapsReducer", "km"),
-    center: select(state, "mapsReducer", "center"),
-    zoom: select(state, "mapsReducer", "zoom"),
+    const { maps, gps, isGps, ganNhat, km, center, zoom, info, listPlayer } =
+        useSelector((state) => ({
+            maps: select(state, "mapsReducer", "maps"),
+            gps: select(state, "mapsReducer", "gps"),
+            isGps: select(state, "mapsReducer", "isGps"),
+            ganNhat: select(state, "mapsReducer", "ganNhat"),
+            km: select(state, "mapsReducer", "km"),
+            center: select(state, "mapsReducer", "center"),
+            zoom: select(state, "mapsReducer", "zoom"),
+            info: select(state, "mapsReducer", "info"),
+            listPlayer: select(state, "mapsReducer", "listPlayer"),
+        }));
 
-  }));
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    const intervalGps = setInterval(() => {
-      dispatch(getGps(navigator))
-    }, 777);
-    return () => clearInterval(intervalGps);
-  }, []);
+    useEffect(() => {
+        const intervalGps = setInterval(() => {
+            dispatch(getGps(navigator));
+        }, 777);
+        return () => clearInterval(intervalGps);
+    }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      dispatch(getKm())
-    }, 3500);
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            dispatch(getKm());
+        }, 3500);
+        return () => clearInterval(interval);
+    }, []);
 
-  useEffect(() => {
-    const test = setInterval(() => {
-      // message.info(,{ marginTop: 50});
-      if(km < 30){
-        message.loading({
-          content: `${ganNhat.name} cách bạn ${(km.toFixed(4)*10000)%1000} m`,
-          style: { marginTop: 60 },
-          duration: 4,
-        })
-      }
-    }, 11111);
-    return () => clearInterval(test);
-  }, [ganNhat]);
+    useEffect(() => {
+        const test = setInterval(() => {
+            // message.info(,{ marginTop: 50});
+            if (km < 30) {
+                message.loading({
+                    content: `${ganNhat.name} cách bạn ${
+                        (km.toFixed(4) * 10000) % 1000
+                    } m`,
+                    style: { marginTop: 60 },
+                    duration: 4,
+                });
+            }
+        }, 11111);
+        return () => clearInterval(test);
+    }, [ganNhat]);
 
-  const onGetGps = () => {
-    dispatch(getGps(navigator));
-  };
+    const onGetGps = () => {
+        dispatch(getGps(navigator));
+    };
 
-  const changeMap = ({ center, zoom, bounds }) => {
-    dispatch(setZooCenter(zoom,center));
-  };
+    const changeMap = ({ center, zoom, bounds }) => {
+        dispatch(setZooCenter(zoom, center));
+    };
 
-  return (
-    <div style={{ height: window.innerHeight - 72, width: "100%" }}>
-      <GoogleMap
-        bootstrapURLKeys={{
-          key: "AIzaSyB-lfJYx98EARbL-SCEWy9TU59USvM3QZY",
-        }}
-        options={{
-          gestureHandling: "greedy",
-          disableDefaultUI: true,
-          fullscreenControl: false,
-          style: [
-            {
-              featureType: "poi",
-              elementType: "labels",
-              stylers: [{ visibility: "off" }],
-            },
-          ],
-        }}
-        center={center}
-        zoom={zoom}
-        gestureHandling={false}
-        onChange={changeMap}
-      >
-        {maps.map((item, index) => {
-          let detail = item;
-          detail.index = index;
-          return (
-            <Marker
-              zoom={zoom}
-              key={index}
-              detail={detail}
-              lat={item.lat}
-              lng={item.lng}
-              onClickMarker={props.onShowDetail}
-            />
-          );
-        })}
-        {gps !== null ? <GpsIcon lat={gps.lat} lng={gps.lng} /> : null}
-      </GoogleMap>
+    const [mapApi, setMapApi] = useState(null);
+    const [mapsApi, setMapsApi] = useState(null);
+    const [mapsLoaded, setMapsLoaded] = useState(false);
 
-      <div
-        style={{
-          position: "relative",
-          float: "right",
-          top: -40,
-          left: -15,
-          height: 30,
-          width: 30,
-          borderRadius: 5,
-          backgroundColor: "#FFF",
-          display: "inline-flex",
-          justifyContent: "center",
-          alignItems: "center",
-          boxShadow:
-            "rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px",
-        }}
-      >
-        <AimOutlined
-          style={{ fontSize: "20px", color: !isGps ? "#7DCBE2" : "#08c" }}
-          onClick={onGetGps}
-        />
-      </div>
-      <ToastContainer />
-    </div>
-  );
+    const onMapLoaded = (mapLoad, mapsLoad) => {
+        if (maps.length > 0 && mapsLoaded === false) {
+            setMapsLoaded(true);
+            setMapApi(mapLoad);
+            setMapsApi(mapsLoad);
+        }
+    };
+
+    const afterMapLoadChanges = () => {
+        if (
+            mapsLoaded &&
+            mapsApi !== null &&
+            mapApi !== null &&
+            maps.length > 0
+        ) {
+            return (
+                <div>
+                    <Polyline map={mapApi} maps={mapsApi} markers={maps} />
+                </div>
+            );
+        }
+    };
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div
+            style={{
+                height: window.innerHeight - 72,
+                width: "100%",
+                position: "absolute",
+            }}
+        >
+            <GoogleMap
+                path={maps}
+                bootstrapURLKeys={{
+                    key: "AIzaSyB-lfJYx98EARbL-SCEWy9TU59USvM3QZY",
+                }}
+                options={{
+                    gestureHandling: "greedy",
+                    disableDefaultUI: true,
+                    disableDoubleClickZoom: true,
+                    streetViewControl: false,
+                    clickableIcons: false,
+                    fullscreenControl: false,
+                    style: [
+                        {
+                            featureType: "poi",
+                            stylers: [{ visibility: "off" }],
+                        },
+                        {
+                            featureType: "transit",
+                            stylers: [{ visibility: "off" }],
+                        },
+                        {
+                            featureType: "landscape",
+                            stylers: [{ visibility: "off" }],
+                        },
+                    ],
+                }}
+                center={center}
+                zoom={zoom}
+                gestureHandling={false}
+                onChange={changeMap}
+                onGoogleApiLoaded={({ map, maps }) => onMapLoaded(map, maps)}
+                yesIWantToUseGoogleMapApiInternals={true}
+            >
+                {mapsLoaded ? afterMapLoadChanges() : null}
+
+                {maps.map((item, index) => {
+                    let detail = item;
+                    detail.index = index;
+                    return (
+                        <Marker
+                            zoom={zoom}
+                            key={index}
+                            detail={detail}
+                            lat={item.lat}
+                            lng={item.lng}
+                            onClickMarker={props.onShowDetail}
+                        />
+                    );
+                })}
+                {gps !== null ? <GpsIcon lat={gps.lat} lng={gps.lng} /> : null}
+            </GoogleMap>
+            <div
+                style={{
+                    position: "relative",
+                    float: "right",
+                    top: -40,
+                    left: -15,
+                    height: 30,
+                    width: 30,
+                    borderRadius: 5,
+                    backgroundColor: "#FFF",
+                    display: "inline-flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    boxShadow:
+                        "rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px",
+                }}
+            >
+                <AimOutlined
+                    style={{
+                        fontSize: "20px",
+                        color: !isGps ? "#7DCBE2" : "#08c",
+                    }}
+                    onClick={onGetGps}
+                />
+            </div>
+            <div
+                onClick={handleOpen}
+                style={{
+                    position: "relative",
+                    float: "right",
+                    top: -window.innerHeight + 82,
+                    left: 20,
+                    height: 40,
+                    width: 70,
+                    borderRadius: 5,
+                    backgroundColor: "#FFF",
+                    display: "inline-flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    boxShadow:
+                        "rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px",
+                }}
+            >
+                <h6>
+                    {!info && maps.length !== 0
+                        ? `0 / ${maps.length}`
+                        : `${info?.count} / ${maps.length}`}
+                </h6>
+            </div>
+            <Modal
+                full
+                open={open}
+                onClose={handleClose}
+                style={{ marginTop: "10vh" }}
+            >
+                <Modal.Header>
+                    <Modal.Title>
+                        <h5>Ranking</h5>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div style={{ height: "40vh" }}>
+                        {listPlayer.map((player, index) => (
+                            <div
+                                key={index}
+                                style={{
+                                    padding: 15,
+                                    marginTop: 10,
+                                    color: "#FFF",
+                                    borderRadius: 10,
+                                    backgroundColor:
+                                        player.userId === info.userId
+                                            ? "#1F7D1F"
+                                            : "#5E219A",
+                                }}
+                            >
+                                <h6>
+                                    {`Top ${index + 1} :  ${player.username}`}
+                                </h6>
+                                <h6>
+                                    {`Đã check in ${player?.count} / ${maps.length} vị trí`}
+                                </h6>
+                            </div>
+                        ))}
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <h6>
+                        {`Bạn đã đã check in tại : ${info?.count} / ${maps.length} vị trí`}
+                    </h6>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 };
 
 export default MapView;
--- resources/js/maps/screen/Maps/components/MarkerDetail.jsx
+++ resources/js/maps/screen/Maps/components/MarkerDetail.jsx
@@ -11,203 +11,204 @@ import { useSelector, useDispatch } from "react-redux";
 const { SubMenu } = Menu;
 
 const MarkerDetail = ({ detail, onShowDetail, isShowDetail }) => {
-  const trId = window.location.search.replace("?tr=", "");
+    const trId = window.location.search.replace("?tr=", "");
 
-  const dispatch = useDispatch();
-  const ref = useRef();
+    const dispatch = useDispatch();
+    const ref = useRef();
 
-  const [updatCheckIn, setUpdatCheckIn] = useState(false);
+    const [updatCheckIn, setUpdatCheckIn] = useState(false);
 
-  const onClose = () => {
-    onShowDetail(false, null);
-    setCurrent("info");
-  };
+    const onClose = () => {
+        onShowDetail(false, null);
+        setCurrent("info");
+    };
 
-  useEffect(() => {
-    setUpdatCheckIn(false);
-  }, [detail]);
+    useEffect(() => {
+        setUpdatCheckIn(false);
+    }, [detail]);
 
-  const handleClick = (e) => {
-    if (updatCheckIn) {
-      dispatch(getMaps(trId));
-    }
-    setUpdatCheckIn(!updatCheckIn);
-  };
+    const handleClick = (e) => {
+        if (updatCheckIn) {
+            dispatch(getMaps(trId));
+        }
+        setUpdatCheckIn(!updatCheckIn);
+    };
 
-  const renderIframeInput = (detail) => {
-    console.log(detail);
-    switch (detail?.input.type) {
-      case "picture":
-        return (
-          <div style={{width: '100vh'}}>
-            <iframe
-              frameBorder="0"
-              style={{
-                position: "relative",
-                height: 300,
-                alignContent: "center",
-                margin: "auto",
-              }}
-              src={`${window.location.origin}/input-picture?tr_id=${trId}&nij=${detail?.index}`}
-              title="picture"
-              allow="camera"
-              width="100%"
-            />
-          </div>
-        );
-      case "audio":
-        return (
-          <div style={{width: '100vh'}}>
-            <iframe
-              frameBorder="0"
-              style={{
-                position: "relative",
-                height: 220,
-                alignContent: "center",
-                margin: "auto",
-              }}
-              src={`${window.location.origin}/input-audio?tr_id=${trId}&nij=${detail?.index}`}
-              title="audio"
-              allow="microphone"
-              width="100%"
-            />
-          </div>
-        );
-      case "qr":
-        return (
-          <div style={{width: '100vh'}}>
-            <iframe
-              onLoad={(e) => console.log(e)}
-              // onSubmit="alert(this.contentWindow.location);"
-              frameBorder="0"
-              style={{ position: "relative",height: 300 }}
-              src={`${window.location.origin}/input-qr?tr_id=${trId}&nij=${detail?.index}`}
-              title="Scan QR code from camera"
-              allow="camera"
-              width="100%"
-            />
-          </div>
-        );
-      case "form":
-        return (
-          <div style={{width: '100vh'}}>
-            <iframe
-              ref={ref}
-              frameBorder="0"
-              style={{
-                position: "relative",
-                height: 280,
-                margin: "auto",
-              }}
-              src={`${window.location.origin}/input-form?tr_id=${trId}&nij=${detail?.index}`}
-              title="Input data from a form"
-              allow="camera"
-              width="100%"
-            />
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
+    const renderIframeInput = (detail) => {
+        switch (detail?.input.type) {
+            case "picture":
+                return (
+                    <div>
+                        <iframe
+                            frameBorder="0"
+                            style={{
+                                position: "relative",
+                                height: window.innerWidth - 34,
+                                borderRadius: 7,
+                            }}
+                            src={`${window.location.origin}/input-picture?tr_id=${trId}&nij=${detail?.index}`}
+                            title="picture"
+                            allow="camera"
+                            width={window.innerWidth - 34}
+                        />
+                    </div>
+                );
+            case "audio":
+                return (
+                    <div>
+                        <iframe
+                            frameBorder="0"
+                            style={{
+                                position: "relative",
+                                height: 212,
+                                borderRadius: 7,
+                            }}
+                            src={`${window.location.origin}/input-audio?tr_id=${trId}&nij=${detail?.index}`}
+                            title="audio"
+                            allow="microphone"
+                            width={window.innerWidth - 34}
+                        />
+                    </div>
+                );
+            case "qr":
+                return (
+                    <div>
+                        <iframe
+                            onLoad={(e) => console.log(e)}
+                            // onSubmit="alert(this.contentWindow.location);"
+                            frameBorder="0"
+                            style={{
+                                position: "relative",
+                                height: (window.innerWidth - 54) > 400 ? 339 : (window.innerWidth - 54)*0.75 + 39,
+                                borderRadius: 7,
+                            }}
+                            src={`${window.location.origin}/input-qr?tr_id=${trId}&nij=${detail?.index}`}
+                            title="Scan QR code from camera"
+                            allow="camera"
+                            width={window.innerWidth - 34}
+                        />
+                    </div>
+                );
+            case "form":
+                return (
+                    <div>
+                        <iframe
+                            ref={ref}
+                            frameBorder="0"
+                            style={{
+                                position: "relative",
+                                height: 276,
+                            }}
+                            src={`${window.location.origin}/input-form?tr_id=${trId}&nij=${detail?.index}`}
+                            title="Input data from a form"
+                            width={window.innerWidth - 34}
+                        />
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
 
-  const renderViewInput = (detail) => {
-    switch (detail?.input.type) {
-      case "picture":
-        return (
-          <div
-            style={{
-              width: window.innerWidth - 34,
-              height: "auto",
-              marginTop: 15,
-            }}
-          >
-            <h6 style={{margin: 10}}>
-              Ảnh bạn đã gửi:
-            </h6>
-            <img src={`${window.location.origin}/${detail.data}`} />
-          </div>
-        );
-      case "audio":
-        return (
-          <div style={{ width: window.innerWidth - 34, marginTop: 15 }}>
-            <h6 style={{margin: 10}}>
-              Audio bạn đã gửi:
-            </h6>
-            <audio controls>
-              <source
-                src={`${window.location.origin}/${detail.data}`}
-                type="audio/ogg"
-              />
-            </audio>
-          </div>
-        );
-      case "qr":
-        return (
-          <div style={{ marginTop: 15 }}>
-            <h6 style={{margin: 10}}>Thông tin QR của bạn : </h6>
-            <Message type="info" style={{ marginBottom: 15 }}>
-              {detail?.data.scanneddata}
-            </Message>
-          </div>
-        );
-      case "form":
-        return (
-          <div style={{ marginTop: 15 }}>
-            <h6 style={{margin: 10}}>Nội dung bạn đã gửi :</h6>
-            <Message type="info" style={{ marginBottom: 15 }}>
-              {detail?.data.comment}
-            </Message>
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
+    const renderViewInput = (detail) => {
+        switch (detail?.input.type) {
+            case "picture":
+                return (
+                    <div
+                        style={{
+                            width: window.innerWidth - 34,
+                            height: "auto",
+                            marginTop: 15,
+                        }}
+                    >
+                        <h6 style={{ margin: 10 }}>Ảnh bạn đã gửi:</h6>
+                        <img src={`${window.location.origin}/${detail.data}`} />
+                    </div>
+                );
+            case "audio":
+                return (
+                    <div
+                        style={{ width: window.innerWidth - 34, marginTop: 15 }}
+                    >
+                        <h6 style={{ margin: 10 }}>Audio bạn đã gửi:</h6>
+                        <audio controls>
+                            <source
+                                src={`${window.location.origin}/${detail.data}`}
+                                type="audio/ogg"
+                            />
+                        </audio>
+                    </div>
+                );
+            case "qr":
+                return (
+                    <div style={{ marginTop: 15 }}>
+                        <h6 style={{ margin: 10 }}>Thông tin QR của bạn : </h6>
+                        <Message type="info" style={{ marginBottom: 15 }}>
+                            {detail?.data.scanneddata}
+                        </Message>
+                    </div>
+                );
+            case "form":
+                return (
+                    <div style={{ marginTop: 15 }}>
+                        <h6 style={{ margin: 10 }}>Nội dung bạn đã gửi :</h6>
+                        <Message type="info" style={{ marginBottom: 15 }}>
+                            {detail?.data.comment}
+                        </Message>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
 
-  return (
-    <div style={{ margin: 17, justifyContent: "center", overflowY: "auto" }}>
-      <h5 style={{ textAlign: "center", marginBottom : 20 }}>{detail?.name}</h5>
-      <div>
-        {!detail?.played || updatCheckIn ? (
-          <div style={{ marginTop: 15 }}>
-            <Message type="warning" style={{ marginBottom: 15 }}>
-              {detail?.desc}
-            </Message>
-            {renderIframeInput(detail)}
-            {updatCheckIn ? (
-              <Button
-                appearance="primary"
-                onClick={handleClick}
-                style={{ marginTop: 15 }}
-              >
-                {!updatCheckIn ? "Update" : "Done"}
-              </Button>
-            ) : null}
-          </div>
-        ) : (
-          <div style={{ marginTop: 15 }}>
-            <Alert
-              message="Bạn đã hoàn thành check in"
-              type="success"
-              showIcon
-            />
-            <Message type="warning" style={{ marginTop: 15 }}>
-              {detail?.desc}
-            </Message>
-            {renderViewInput(detail)}
-            <Button
-              appearance="ghost"
-              onClick={handleClick}
-              style={{ marginTop: 15 }}
-            >
-              Update
-            </Button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div
+            style={{ margin: 17, justifyContent: "center", overflowY: "auto" }}
+        >
+            <h5 style={{ textAlign: "center", marginBottom: 20 }}>
+                {detail?.name}
+            </h5>
+            <div>
+                {!detail?.played || updatCheckIn ? (
+                    <div style={{ marginTop: 15 }}>
+                        <Message type="warning" style={{ marginBottom: 15 }}>
+                            {detail?.desc}
+                        </Message>
+                        {renderIframeInput(detail)}
+                        {updatCheckIn ? (
+                            <Button
+                                appearance="primary"
+                                onClick={handleClick}
+                                style={{ marginTop: 15 }}
+                            >
+                                {!updatCheckIn ? "Update" : "Done"}
+                            </Button>
+                        ) : null}
+                    </div>
+                ) : (
+                    <div style={{ marginTop: 15 }}>
+                        <Alert
+                            message="Bạn đã hoàn thành check in"
+                            type="success"
+                            showIcon
+                        />
+                        <Message type="warning" style={{ marginTop: 15 }}>
+                            {detail?.desc}
+                        </Message>
+                        {renderViewInput(detail)}
+                        <Button
+                            appearance="ghost"
+                            onClick={handleClick}
+                            style={{ marginTop: 15 }}
+                        >
+                            Update
+                        </Button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default MarkerDetail;
